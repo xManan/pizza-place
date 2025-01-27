f017f6c0e51c2d7adb27dd1a51077ee6efccd2ad
manan <mananchawla10@gmail.com>
2025-01-27 12:47:56 +0000

    updated htmx attr

--- resources/views/components/quantity-btn.blade.php
+++ resources/views/components/quantity-btn.blade.php
@@ -1,6 +1,6 @@
 @props(['id', 'isCheckout', 'deliverTo'])
 <div class="w-16 text-center border border-green-500 rounded text-green-500 px-2">
-    <button hx-patch="/cart/item/{{ $id }}/qty/-?checkout={{ $isCheckout }}&deliverTo={{ $deliverTo }}" hx-target="#cart" hx-include="[name=_token]" hx-swap="outerHTML" class="text-lg">-</button>
+    <button hx-patch="{{ url('/cart/item/' . $id . '/qty/-?checkout=' . $isCheckout . '&deliverTo=' . $deliverTo) }}" hx-target="#cart" hx-include="[name=_token]" hx-swap="outerHTML" class="text-lg">-</button>
     <span class="font-bold text-xs">{{ $slot }}</span>
-    <button hx-patch="/cart/item/{{ $id }}/qty/+?checkout={{ $isCheckout }}&deliverTo={{ $deliverTo }}" hx-target="#cart" hx-include="[name=_token]" hx-swap="outerHTML" class="text-lg">+</button>
+    <button hx-patch="{{ url('/cart/item/' . $id / '/qty/+?checkout=' . $isCheckout . '&deliverTo=' . $deliverTo) }}" hx-target="#cart" hx-include="[name=_token]" hx-swap="outerHTML" class="text-lg">+</button>
 </div>
