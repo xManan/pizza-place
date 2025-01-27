e4f9fe752acbb5ae902705063347d0a6a8033bd5
manan <mananchawla10@gmail.com>
2025-01-27 13:26:14 +0000

    update

--- resources/views/components/menu/item-options.blade.php
+++ resources/views/components/menu/item-options.blade.php
@@ -21,7 +21,7 @@
     </script>
     <div class="fixed top-0 left-0 z-10 w-full h-full bg-black/25" x-on:click="document.getElementById('menu-item-options').remove();scrollOff=false;">
     <div x-on:click="$event.stopPropagation();" class="fixed min-w-96 border-2 border-orange-500 bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg p-4">
-        <form action="{{ url('/cart') }}" method="POST">
+        <form action="{{ parse_url(url('/cart'), PHP_URL_PATH) }}" method="POST">
             @csrf
             <h2 class="text-2xl font-bold">{{ $item->name }} ({{ 'Rs. ' . $item->base_price / 100 }})</h2>
             <hr>
